e4ff2dff44f03821d52f1ba80317bdba15d742d9
first commit <codrex123@gmail.com>
2016-02-17 12:07:29 -0500

    Scenario where player is on top of a moving platform / or jumping on top

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -62,9 +62,7 @@ public class PlatformController : RaycastController {
 				Vector2 rayOrigin = (directionX == -1)?raycastOrigins.bottomLeft:raycastOrigins.bottomRight;				
 				rayOrigin += Vector2.up * (horizontalRaySpacing * i);				
 				RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, passengerMask);
-				
-				
-				
+					
 				if (hit) {
 					if (!movedPassengers.Contains(hit.transform)) {
 						movedPassengers.Add(hit.transform);
@@ -74,11 +72,31 @@ public class PlatformController : RaycastController {
 						hit.transform.Translate(new Vector3(pushX, pushY));
 					}		
 				}	
-				
-				
-				
+		
+			}	
+		}
+		
+		
+		// Passenger on top of a horizonrally or downward moving platform
+		if (directionY == -1 || velocity.y == 0 && velocity.x != 0) {
+		
+			float rayLength = skinWidth * 2;
+			
+			for (int i =0; i < verticalRayCount; i++) {
+				Vector2 rayOrigin = raycastOrigins.topLeft + Vector2.right * (verticalRaySpacing * i);		
+				RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, passengerMask);
+								
+				if (hit) {
+					if (!movedPassengers.Contains(hit.transform)) {
+						movedPassengers.Add(hit.transform);
+						float pushX = velocity.x;
+						float pushY = velocity.y;
+						
+						hit.transform.Translate(new Vector3(pushX, pushY));
+					}		
+				}	
 			}
-				
+		
 		}
 	
 	
