4aa78a42219f9fe136e5e87ff4f52398106064a9
first commit <codrex123@gmail.com>
2016-02-17 11:58:15 -0500

    Horizontally moving platforms

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -49,12 +49,38 @@ public class PlatformController : RaycastController {
 						float pushY = velocity.y - (hit.distance - skinWidth) * directionY;
 						
 						hit.transform.Translate(new Vector3(pushX, pushY));
-					}
-					
+					}		
 				}	
-				
 			}
 		}
+	
+		// Horizontally moving platform
+		if (velocity.x != 0) {
+		
+			float rayLength = Mathf.Abs (velocity.x) + skinWidth;	
+			for (int i =0; i < horizontalRayCount; i++) {			
+				Vector2 rayOrigin = (directionX == -1)?raycastOrigins.bottomLeft:raycastOrigins.bottomRight;				
+				rayOrigin += Vector2.up * (horizontalRaySpacing * i);				
+				RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, passengerMask);
+				
+				
+				
+				if (hit) {
+					if (!movedPassengers.Contains(hit.transform)) {
+						movedPassengers.Add(hit.transform);
+						float pushX = velocity.x - (hit.distance - skinWidth) * directionX;
+						float pushY = 0;
+						
+						hit.transform.Translate(new Vector3(pushX, pushY));
+					}		
+				}	
+				
+				
+				
+			}
+				
+		}
+	
 	
 	
 	}
