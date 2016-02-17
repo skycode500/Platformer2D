92f60ada84b4a8df2440cd2b8ef61bef0172eca6
first commit <codrex123@gmail.com>
2016-02-17 10:43:30 -0500

    (E6- Moving platform basics) - create superclass RaycastController
    Controller2d and platform controller will inherit from it.

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -1,37 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (BoxCollider2D))]
-public class Controller2D : MonoBehaviour {
+public class Controller2D : RaycastController {
 
-
-	public LayerMask collisionMask;
-	
-	const float skinwidth = .015f;
-	public int horizontalRayCount = 4;
-	public int verticalRayCount = 4;
-	
 	float maxClimbAngle = 80;
 	float maxDescendAngle = 75;
 	
-	float horizontalRaySpacing;
-	float verticalRaySpacing;
-	
-	BoxCollider2D collider;
-	RaycastOrigins raycastOrigins;
 	public CollisionInfo collisions;
 	
 	
-	void Start() {
-		collider = GetComponent<BoxCollider2D>(); 
-		
-		CalculateRaySpacing ();
-		
+	
+	public override void Start() {
+		base.Start();
 	}
-	
-	
-	
-	
 	
 	
 	public void Move(Vector3 velocity) {
@@ -233,42 +214,7 @@ public class Controller2D : MonoBehaviour {
 		}
 	}
 	
-	
-	void UpdateRaycastOrigins() {
-		Bounds bounds = collider.bounds;
-		bounds.Expand (skinwidth * -2);
-		
-		raycastOrigins.bottomLeft = new Vector2 (bounds.min.x, bounds.min.y);
-		raycastOrigins.bottomRight = new Vector2 (bounds.max.x, bounds.min.y);
-		raycastOrigins.topLeft = new Vector2 (bounds.min.x, bounds.max.y);
-		raycastOrigins.topRight = new Vector2 (bounds.max.x, bounds.max.y);
-		
-		
-	}
-	
-	void CalculateRaySpacing() {
-		Bounds bounds = collider.bounds;
-		bounds.Expand (skinwidth * -2);
-		
-		// make sure ray count is at least 2
-		horizontalRayCount = Mathf.Clamp (horizontalRayCount, 2, int.MaxValue);
-		verticalRayCount = Mathf.Clamp (verticalRayCount, 2, int.MaxValue);
-		
-	
-		// 11:00 EP1
-		horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
-		verticalRaySpacing = bounds.size.x / (verticalRayCount -1);
-	
-	}
-	
-	
-	
-	struct RaycastOrigins {
-		public Vector2 topLeft, topRight;
-		public Vector2 bottomLeft, bottomRight;
-	
-	}
-	
+
 	public struct CollisionInfo {
 		public bool above, below;
 		public bool left, right;
