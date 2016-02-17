aabaa20245d66916be327cc8666aeadcfadb5c3b
first commit <codrex123@gmail.com>
2016-02-17 11:03:52 -0500

    MovePassenger method

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlatformController : RaycastController {
 
 
+	public LayerMask passengerMask;
 	public Vector3 move;
 	
 	public override void Start () {
@@ -12,7 +13,58 @@ public class PlatformController : RaycastController {
 	
 	// Update is called once per frame
 	void Update () {
+	
+		UpdateRaycastOrigins ();
+	
 		Vector3 velocity = move * Time.deltaTime;
+		
+		MovePassengers(velocity);
 		transform.Translate (velocity);
 	}
+	
+	void MovePassengers(Vector3 velocity) {
+	
+		float directionX = Mathf.Sign (velocity.x);
+		float directionY = Mathf.Sign (velocity.y);
+		
+		// Vertically moving platform 
+		// e6 - 8
+		if (velocity.y != 0) {
+			float rayLength = Mathf.Abs (velocity.y) + skinwidth;
+				
+			for (int i =0; i < verticalRayCount; i++) {
+				Vector2 rayOrigin = (directionY == -1)?raycastOrigins.bottomLeft:raycastOrigins.topLeft;
+				rayOrigin += Vector2.right * (verticalRaySpacing * i);		
+				RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, passengerMask);
+				
+			
+				
+				// e6 - 10
+				if (hit) {
+					float pushX = (directionX == 1)?velocity.x:0;
+					float pushY = velocity.y - (hit.distance - skinwidth) * directionY;
+					
+					hit.transform.Translate(new Vector3(pushX, pushY);
+					
+				}	
+				
+			}
+		}
+	
+	
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
