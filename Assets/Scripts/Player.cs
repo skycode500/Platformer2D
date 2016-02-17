f1699f29c178739b3019f6f776c611a485ac7795
first commit <codrex123@gmail.com>
2016-02-16 20:30:25 -0500

    jump on space press

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,10 +8,12 @@ public class Player : MonoBehaviour {
 
 
 
+
 	float moveSpeed = 6;
 	
 	float gravity = -20;
 	Vector3 velocity;
+	float jumpVelocity = 8;
 
 
 
@@ -34,6 +36,15 @@ public class Player : MonoBehaviour {
 	
 	
 		Vector2 input = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		
+		
+		// jump on space press
+		if (Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
+			velocity.y = jumpVelocity;
+		}
+		
+		
+		
 	
 		velocity.x = input.x * moveSpeed;
 		
